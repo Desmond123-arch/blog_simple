518be1a6f1a4859bd73a79b9e699ff9f25a3aa7f
desmends27 <desmondmends27@gmail.com>
2024-09-23 00:22:25 +0000

    added error element

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import MainLayout from "./components/Layout/MainLayout"
 import { mainRoutes } from "./routes"
+import ErrorPage from "./components/404page"
 
 function App() {
 
   const router = createBrowserRouter([
     {
       element:<MainLayout/>,
+      errorElement: <ErrorPage/>,
       children: mainRoutes
     }
   ])
---
+++ src/components/404page.jsx
@@ -0,0 +1,7 @@
+const ErrorPage = () => {
+    return (
+        <p className="text-bold font-5xl mx-auto">You didn't ask for this</p>
+    );
+}
+ 
+export default ErrorPage;
